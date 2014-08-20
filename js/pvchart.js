42cae95e656195cfef2ac046c025c604de10a855
thfield <thfield@gmail.com>
2014-08-20 14:30:55 -0700

    datum chart working

--- js/pvchart.js
+++ js/pvchart.js
@@ -1,13 +1,30 @@
+
 function drawChart(containerId, dataPath, chartOptions) {
   d3.json(dataPath, function(data) {
     //console.log(data);
     
-    nv.addGraph(function() {  
-      //var chart = nv.models.multiBarChart()
-      var chart = nv.models.discreteBarChart()
-          .x(function(d) { return d.label })
-          .y(function(d) { return d.value })
-          ;
+    nv.addGraph(function() {
+      if (chartOptions.style = 'discrete'){
+        var chart = nv.models.discreteBarChart();  
+      } else {
+        var chart = nv.models.multiBarChart();
+      }
+      
+      chart  
+        .x(function(d) { return d.label })
+        .y(function(d) { return d.value })
+        ;
+
+      chart.xAxis.rotateLabels(-45);
+      // http://stackoverflow.com/questions/13136964/how-can-i-position-rotated-x-axis-labels-on-column-chart-using-nvd3
+/*
+      var xTicks = d3.select('.nv-x.nv-axis > g').selectAll('g');
+      xTicks
+        .selectAll('text')
+        .style("text-anchor", "end")
+        .attr('transform', function(d,i,j) { return 'translate (-5, 0) rotate(-45 0,0)' }) ;
+*/
+      chart.margin({bottom:75});
 
       chart.yAxis.tickFormat(d3.format(',f'));    
       chart.options(chartOptions);    
@@ -23,55 +40,21 @@ function drawChart(containerId, dataPath, chartOptions) {
   });
 }
 
-
-
-
 var opt1 = {
-  color: ["#edf8fb","#ccece6","#99d8c9","#66c2a4","#2ca25f","#006d2c"],
-  staggerLabels: true,
-  tooltips: true,
-  showValues: true,
-  valueFormat: d3.format(',f')
-};
+    color: ["#edf8fb","#ccece6","#99d8c9","#66c2a4","#2ca25f","#006d2c"],
+    staggerLabels: true,
+    tooltips: true,
+    showValues: true,
+    valueFormat: d3.format(',f'),
+    style: 'discrete'
+},
+  opt2 = {
+   // xAxis.rotateLabels(-45)
+   tooltips: true,
+   showControls: false,
+   style : 'multi'
+};  
 
-drawChart('#chart', 'data/pv.json', opt1);
-
-
-
-
-
-
-inData =  
-[ 
-  {
-    "key": "Orgs in each State",
-    "values" : [
-      
-      {
-        "label": "Alabama",
-        "value": "13",
-      },
-      
-      {
-        "label": "Alaska",
-        "value": "14",
-      },
-      
-      {
-        "label": "Arizona",
-        "value": "16",
-      },               
-    ]
-  }
-];
-
-
-
-
-
-
-
-
-
-
-
+drawChart('#chart1', 'data/pv.json', opt1);
+drawChart('#chart2', 'data/pv.json', opt2);
+  
---
+++ js/pvdatum.js
@@ -0,0 +1,44 @@
+var opts= {
+	width: 400,
+    height: 40
+};
+
+datum("#fact", 'data/pvdatum.json', opts);
+
+
+
+function datum(containerID, dataPath, chartOptions){
+	d3.json(dataPath, function(data) {
+	    //console.log(data);
+	    //var fact = data.title;
+	    //console.log(data.value);
+
+	    data.value= numberWithCommas(data.value);
+
+	    var svg=d3.select(containerID)
+	    		.attr("width", chartOptions.width)
+	    		.attr("height", chartOptions.height)
+	    		.append("g");
+
+	    svg.append("text")      	
+	      	.attr("class", "datum_label")
+	       	.attr("x", chartOptions.width/2 + 10)
+	      	.attr("y", chartOptions.height)
+	      	//.attr("dy", height)
+	      	.style("text-anchor", "beginning")
+	      	.text(data.title);
+
+	    svg.append("text")
+	    	.attr("class", "datum_fact")
+	    	.attr("x", chartOptions.width/2)
+	      	.attr("y", chartOptions.height)
+	      	.style("text-anchor", "end")
+	    	.text(data.value);
+	});
+	
+};
+
+function numberWithCommas(x) {
+	// http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	}
